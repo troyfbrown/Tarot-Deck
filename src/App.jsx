8aa96f3b92d4cac38cbf8ef74cb185f791d83cc0
Troy Brown <troyfbrown@gmail.com>
2025-08-04 17:25:01 -0400

    major arcana titles added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import CardDisplay from './CardDisplay'
 import './App.css'
 
 function App() {
@@ -9,25 +10,34 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Welcome To The Tarot Deck</h1>
+        <h2>Major Arcana</h2>
+        <div class="card-container">
+          <CardDisplay id="0" title="The Fool"/>
+          <CardDisplay id="1" title="The Magician"/>
+          <CardDisplay id="2" title="High Priestess"/>
+          <CardDisplay id="3" title="The Empress"/>
+          <CardDisplay id="4" title="The Emperor"/>
+          <CardDisplay id="5" title="High Priest"/>
+          <CardDisplay id="6" title="The Lovers"/>
+          <CardDisplay id="7" title="The Chariot"/>
+          <CardDisplay id="8" title="Strength"/>
+          <CardDisplay id="9" title="The Hermit"/>
+          <CardDisplay id="10" title="The Wheel of Fortune"/>
+          <CardDisplay id="11" title="Justice"/>
+          <CardDisplay id="12" title="The Hanged Man"/>
+          <CardDisplay id="13" title="Death"/>
+          <CardDisplay id="14" title="Temperance"/>
+          <CardDisplay id="15" title="Chains"/>
+          <CardDisplay id="16" title="The Tower"/>
+          <CardDisplay id="17" title="The Star"/>
+          <CardDisplay id="18" title="The Moon"/>
+          <CardDisplay id="19" title="The Sun"/>
+          <CardDisplay id="20" title="Judgement"/>
+          <CardDisplay id="21" title="The World"/>
+        </div>
+        
       </div>
-      <h1>Vite + React This works</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
